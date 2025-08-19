eab77dad3a95bdf3fa9c2e950aa57a1ba8c5409b
HenryPham0706 <phamngocduy07062023@gmail.com>
2025-08-19 19:33:42 +0700

    Add files via upload

---
+++ baitapquanlidonbanhang.dart
@@ -0,0 +1,34 @@
+
+import 'dart:io';
+
+void main() {
+String tenSanPham = stdin.readLineSync()!;
+double soLuongMua = double.parse( stdin.readLineSync()!);
+double donGia = double.parse(stdin.readLineSync()!);
+double thanhTien = 0 ;
+thanhTien = soLuongMua * donGia ;
+thue = 0;
+print ('tong tien la :$thanhTien');
+if (thanhTien > 1_000_000){
+print ('giam gia 10% la :${thanhTien/10}')
+  thanhTien  = thanhTien  - thanhTien /10;
+  thue = 8*thanhTien/100;
+ print ('thue VAT :$thue');
+}
+
+if (thanhTien  >= 500_000 && thanhTien <1_000_000){
+ print ('giam gia 5% la :${thanhTien/20}')
+  thanhTien = thanhTien - thanhTien/20;
+  thue = 8*thanhTien/100;
+  print ('thue VAT :$thue');
+}
+
+else {
+  print ('khong giam gia');
+  thue = 8*thanhTien/100;
+  print ('thue VAT :$thue');
+}
+print ('thanh toan :$thanhTien');
+
+print ('tong thanh toan cuoi cung :${thanhTien+thue} ');
+}
