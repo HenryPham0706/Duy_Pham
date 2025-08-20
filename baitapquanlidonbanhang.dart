50d90248664a911beaa0462e92d053d238964cdf
HenryPham0706 <phamngocduy07062023@gmail.com>
2025-08-20 19:19:16 +0700

    Update baitapquanlidonbanhang.dart

--- baitapquanlidonbanhang.dart
+++ baitapquanlidonbanhang.dart
@@ -7,17 +7,17 @@ double soLuongMua = double.parse( stdin.readLineSync()!);
 double donGia = double.parse(stdin.readLineSync()!);
 double thanhTien = 0 ;
 thanhTien = soLuongMua * donGia ;
-thue = 0;
+double thue = 0;
 print ('tong tien la :$thanhTien');
 if (thanhTien > 1_000_000){
-print ('giam gia 10% la :${thanhTien/10}')
+print ('giam gia 10% la :${thanhTien/10}');
   thanhTien  = thanhTien  - thanhTien /10;
   thue = 8*thanhTien/100;
  print ('thue VAT :$thue');
 }
 
-else if (thanhTien  >= 500_000 && thanhTien <1_000_000){
- print ('giam gia 5% la :${thanhTien/20}')
+if (thanhTien  >= 500_000 && thanhTien <1_000_000){
+ print ('giam gia 5% la :${thanhTien/20}');
   thanhTien = thanhTien - thanhTien/20;
   thue = 8*thanhTien/100;
   print ('thue VAT :$thue');
@@ -33,3 +33,4 @@ print ('thanh toan :$thanhTien');
 print ('tong thanh toan cuoi cung :${thanhTien+thue} ');
 
 }
+
